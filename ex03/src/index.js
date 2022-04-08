1458e6e2d486897d08ee111acec29c18f52d7591
Rima-Mec <dmerima08@gmail.com>
2022-04-08 09:11:18 +0200

    Addin .index.js file

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+// Create myPetsArray here
+var myPetsArray = ["Dog", "Cat"];
+// End of myPetsArray here
+// Create a function below this line
+function myArrayFunction(myPets) {
+myPetsArray = myPets = myNewPets;
+myPets.push([Bird, Fish])
+}
+// End of creating a function
+console.log(myArrayFunction(myPets));
+module.exports = myArrayFunction;
