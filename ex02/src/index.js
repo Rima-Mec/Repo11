61907d034bcc9a6e31ed54ced2fe15bcc9280927
Rima-Mec <dmerima08@gmail.com>
2022-04-10 17:05:58 +0200

    Adding index.js file

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,20 +1,24 @@
 // Only change code below this code
 var myStr = "Paragon is the best!"
 function useMethods(str) {
-    var myReverse;
-    var counterCharacters;
-    //
-    myReverse = myStr.split(""); // [ "P","a","r","a","g","o","n"; "i","s", "t","h","e", "b","e","s","t","!"]
-    //  
-    myStr =myStr.reverse(); 
-    countCharacters = myStr.map(()=>s.length);
+    myStr=str;
+    var myReverse; 
+    var countCharacters;
+
+    myStr=str.split(""); // [ "Paragon" "is" "the" "best!"]
+ 
+    myReverse=myStr.reverse(); // ["!tseb eht si nogaraP"]
+
+    countCharacters=myReverse.join(""); // "!tseb eht si nogaraP"
+
+    countCharacters =myReverse.map((s)=>s.length);
+
     myStr=myStr.map((r)=>r.split("").reverse().join(""));
-    myReverse = myStr.join("");
     return myReverse, countCharacters;
 
 };
 
-
 // Only change code above this code
+
 console.log(useMethods(myStr));
-module.export = useMethods;
+module.exports = useMethods;
